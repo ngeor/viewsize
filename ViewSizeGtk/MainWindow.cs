3455961a4a7f43060652177f3322387fc0fcc2ec
Nikolaos Georgiou <ngeor@crlflabs.com>
2017-09-01 15:14:58 +0200

    Added GTK# version

---
+++ ViewSizeGtk/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnSelectFolder(object sender, EventArgs e)
+    {
+        MessageDialog m = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, " Select a folder");
+        m.Run();
+        m.Destroy();
+    }
+
+    protected void OnScanFolder(object sender, EventArgs e)
+    {
+    }
+}
