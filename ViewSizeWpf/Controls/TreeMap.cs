f51441d5f87f20623759ef00e72ad4b30c590ee7
Nikolaos Georgiou <ngeor@crlflabs.com>
2017-09-04 21:35:01 +0200

    Adding TreeMap control for WPF

---
+++ ViewSizeWpf/Controls/TreeMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ViewSizeWpf.Controls
+{
+    public class TreeMap : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            drawingContext.DrawRectangle(Brushes.AliceBlue, new Pen(Brushes.Black, 1), new Rect(0, 0, ActualWidth, ActualHeight));
+        }
+    }
+}
