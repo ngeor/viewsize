56c28d7dbed2f4bf2bcde48b86be4a49dca9d552
Nikolaos Georgiou <ngeor@crlflabs.com>
2017-09-07 21:13:36 +0200

    Adding unit test project

---
+++ ViewSize.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using CRLFLabs.ViewSize.TreeMap;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewSize.Tests
+{
+    public class SizeFTest
+    {
+        [Test]
+        public void Create()
+        {
+            var s = new SizeF(100, 200);
+            Assert.AreEqual(100, s.Width);
+            Assert.AreEqual(200, s.Height);
+        }
+    }
+}
