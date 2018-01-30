56d64704093597b9b70b09e1a272c6816d20646c
Nikolaos Georgiou <ngeor@crlflabs.com>
2018-01-30 20:21:45 +0100

    Upgraded testing dependencies.

--- coverage.ps1
+++ coverage.ps1
@@ -1,2 +1,7 @@
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -filter:"+[ViewSize.Tests]*" -target:.\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe -targetargs:"--where:cat!=Performance --workers=1 .\ViewSize.Tests\bin\Debug\ViewSize.Tests.dll"
-.\packages\ReportGenerator.3.0.2\tools\ReportGenerator.exe -reports:results.xml -targetdir:coveragereport
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user `
+	-output:opencover.xml `
+	-filter:"+[ViewSize.Tests]*" `
+	-target:.\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe `
+	-targetargs:"--where:cat!=Performance --workers=1 .\ViewSize.Tests\bin\Debug\ViewSize.Tests.dll"
+
+.\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe -reports:opencover.xml -targetdir:coverage
